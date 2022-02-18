edd5ad9a03f66f31862a2282968f0df3f6311ae3
John <johnmagdy@rocketmail.com>
2022-02-18 22:54:58 +0200

    added custom style and put comments

--- src/components/service_carousel.js
+++ src/components/service_carousel.js
@@ -1,47 +1,48 @@
-import React from "react";
-import { Carousel } from "react-responsive-carousel";
-import { useState, useEffect } from "react";
+import React, {useEffect, useState} from "react";
+import {Carousel} from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
-export default ({ service_id }) => {
-const [error, setError] = useState(null);
-  const [isLoaded, setIsLoaded] = useState(false);
-  const [items, setItems] = useState([]);
+import 'bootstrap/dist/css/bootstrap.css';
+import './carousel_custom.css';
 
-  // Note: the empty deps array [] means
-  // this useEffect will run once
-  // similar to componentDidMount()
-  useEffect(() => {
-    fetch("http://127.0.0.1:8000/images/")
-      .then(res => res.json())
-      .then(
-        (result) => {
-          setIsLoaded(true);
-          setItems(result.images);
-        },
-        // Note: it's important to handle errors here
-        // instead of a catch() block so that we don't swallow
-        // exceptions from actual bugs in components.
-        (error) => {
-          setIsLoaded(true);
-          setError(error);
-        }
-      )
-  }, [])
-    console.log(items)
-    console.log(service_id)
-  return(
-      <div className="">
-        <Carousel autoPlay>
-          {items.map(item => {
-              if (item.sv_id === service_id){
-                  return(
-                      <div>
-            <img alt=""  src={`http://127.0.0.1:8000${item.image}`}/>
-            <p className="legend">{item.title}</p>
-          </div>)
-              }
-        })}
-        </Carousel>
-      </div>
-  );
+export default ({service_id}) => {
+    const [error, setError] = useState(null);
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [items, setItems] = useState([]);
+    //fetch backend api for images
+    useEffect(() => {
+        fetch("http://127.0.0.1:8000/images/")
+            .then(res => res.json())
+            .then(
+                (result) => {
+                    setIsLoaded(true);
+                    setItems(result.images);
+                },
+                (error) => {
+                    setIsLoaded(true);
+                    setError(error);
+                }
+            )
+    }, [])
+    // console.log(items)
+    // console.log(service_id)
+    // console.log(error)
+    return (
+        <div className="container-xl">
+            <h1>Carousel</h1>
+           {/*responsive carousel component */}
+            <Carousel autoPlay width="100%" dynamicHeight={true} showStatus={false}>
+                {/*load only images for specific services*/}
+                {isLoaded && items.map(item => {
+                    if (item.sv_id === service_id) {
+                        return (
+                            <div>
+                                <img alt={item.alt} src={`http://127.0.0.1:8000${item.image}`}/>
+                                {/*render legend when title is valid*/}
+                                {item.title && <p className="legend">{item.title}</p>}
+                            </div>)
+                    }
+                })}
+            </Carousel>
+        </div>
+    );
 }
