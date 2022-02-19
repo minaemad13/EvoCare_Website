e6d3245d59e72524a4c63faa208269007c04484a
minaemad13 <menaemadorai@gmail.com>
2022-02-19 12:29:52 +0200

    make documentation for this component

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,20 +1,22 @@
 import React, { useEffect } from 'react';
-import { Datepicker} from '@mobiscroll/react';
+import { Datepicker } from '@mobiscroll/react';
 import '@mobiscroll/react/dist/css/mobiscroll.min.css';
 import { useState } from 'react';
 import axios from 'axios';
 import moment from 'moment'
+import background from "./carbon2.jpg"
 import { useLocation } from "react-router-dom";
 import { FaCalendarCheck, FaMoneyCheck } from 'react-icons/fa';
 
 import emailjs from "emailjs-com"
 
+// git current date in this format YYY-MM-DDT00:00 (iso8601)
 const today = moment();
 const date = today.format('YYYY-MM-DD') + "T00:00"
 
-
+// this component to reserve appointement 
 const Book = () => {
-
+  // initialize states for selected date , invalid dates , pachage price , form data
   const [selected, setSelected] = useState([]);
   const [invalid, setInvalid] = useState([]);
   const [package_price, setPackage_price] = useState([]);
@@ -31,17 +33,18 @@ const Book = () => {
     EmailErr: "",
     PhoneErr: "",
   });
-
   var myinvalid = []
+  var Tax = 50
+
+
   const userid = 1
- // const location = useLocation();
+  // const location = useLocation();
+  // useEffect(() => {
+  //    setPackage_price(location.state.price);
+  //    setPackage_price(location.state.id);
+  // }, [location]);
 
-    // useEffect(() => {
-    //    setPackage_price(location.state.price);
-    //    setPackage_price(location.state.id);
-    // }, [location]);
-
-  useEffect(
+  useEffect(  // get the booked date from database when the value of selected date changed useing react hook on update  
     () => {
       axios.get('http://127.0.0.1:8000/invalid/')
         .then(function (response) {
@@ -53,21 +56,19 @@ const Book = () => {
     },
     [selected],
   );
-  invalid.map((inv) => {
+
+  invalid.map((inv) => {         // loop on data that get from database to push it in myivalid list to represent it 
     //console.log(inv.Date_Time)
     myinvalid.push({ start: inv.Date_Time, end: inv.Date_Time })
   });
-  const onSelectedChange = (ev) => {
-    setSelected(ev.value);
-  };
-  //******************************************** */
-  const handleSubmit = (e) => {
+
+  const handleSubmit = (e) => {   // take the form values and selected date and time from calender and send it to backend using post request 
     e.preventDefault();
     emailjs.sendForm("service_fh510hk",
-     "template_mgopfxs", e.target, 
-     "user_Qdr9KNtFvRj7X19Pg2l5x").then((res)=>{console.log(res)}).catch(err=>{
-          console.log(err)
-     })
+      "template_mgopfxs", e.target,
+      "user_Qdr9KNtFvRj7X19Pg2l5x").then((res) => { console.log(res) }).catch(err => {
+        console.log(err)
+      })
     axios.post('http://127.0.0.1:8000/book', {
 
       "Date_Time": selected,
@@ -87,7 +88,14 @@ const Book = () => {
     // console.log(FormData['First_Name']);
   };
 
-  const FormHandeller = (e) => {
+
+
+  const onSelectedChange = (ev) => { // set Date and time using set state when value change in Calender 
+    setSelected(ev.value);
+  };
+
+
+  const FormHandeller = (e) => { // this functio that take data from form set it in state using setstate and take the value make the custum validation on it 
     if (e.target.name === "First_Name") {
       setFormData({
         ...FormData,
@@ -135,76 +143,82 @@ const Book = () => {
 
     }
   }
-  
+
+
+
+  // backgroundImage:`url(${background})` , backgroundrepeat: "no-repeat" 
   return (
-    <div style={{ backgroundColor: 'black' }}>
-      <h1 className='text-center' style={{ color: "orange" }}>Date and Time</h1>
-      <div className='m-5' style={{fontSize:"20px"}}>
-        <Datepicker
-          controls={['calendar', 'timegrid']}
-          min={date}
-          max="2222-12-11T00:00"
-          stepMinute={60}
-          minTime="10:00"
-          maxTime="22:00"
-          display='inline'
-          returnFormat='iso8601'
-          theme="material-dark"
-          themeVariant='dark'
-          invalid={myinvalid}
-          onChange={onSelectedChange}
-        />
-      </div>
-      <br></br>
-      <div className="container ">
-        <div className='row'>
-          <div className='col-6'>
-            <div className='row' style={{ marginTop: "50px" }}>
-              <FaCalendarCheck width={200} height={50} color='orange' />
-              <br></br>
-              <p style={{ color: 'orange' }}>Booking Date and Time </p>
-              <h3 style={{ color: "orange" }}> {selected}</h3>
-            </div>
-            <div className='row' style={{ marginTop: "50px" }}>
-              <FaMoneyCheck width={200} height={50} color='orange' />
-              <br></br>
-              <p style={{ color: 'orange' }}>Reservation Tax</p>
-              <h3 style={{ color: 'orange' }}>50 EGP</h3>
-            </div>
-          </div>
-          <div className='col-6' style={{ backgroundColor: "black", height: "400px" }}>
-            <h3 style={{ color: "white", marginTop: "20px" }}>Please input your <span style={{ color: "orange" }}>contact details</span></h3>
-            <div style={{ marginTop: "50px" }}>
-
-              <form onSubmit={(e) => handleSubmit(e)}>
-                <div className='row'>
-                  <div className="col-6">
-
-                    <input type="text" placeholder='First Name*' className="form-control" value={FormData.Name} name="First_Name" onChange={(e) => FormHandeller(e)} />
-                    <small className="text-danger">{FormErr.First_NameErr}</small>
-                  </div>
-                  <div className="col-6">
-
-                    <input type="text" placeholder='Last Name*' className="form-control" value={FormData.Name} name="Last_Name" onChange={(e) => FormHandeller(e)} />
-                    <small className="text-danger">{FormErr.Last_NameErr}</small>
-                  </div>
-                </div>
+    <div style={{ backgroundColor: "#1C1C1C" }}>
+      <div className='container'>
+        <h1 className='text-center' style={{ color: "orange" }}>Date and Time</h1>
+        <div className='m-5' style={{ border: "2 orange solid" }}>
+          <Datepicker  //using the DatePicker Component using mobiscroll that can take Date and Time
+            controls={['calendar', 'timegrid']}
+            min={date}
+            max="2222-12-11T00:00"
+            stepMinute={60}
+            minTime="10:00"
+            maxTime="22:00"
+            display='inline'
+            returnFormat='iso8601'
+            theme="ios-dark"
+            themeVariant='dark'
+            invalid={myinvalid}
+            onChange={onSelectedChange}
+          />
+        </div>
+        <hr style={{ color: "orange", height: "2px" }}></hr>
+        <div className="container ">
+          <div className='row'>
+            <div className='col-6'>
+              <div className='row' style={{ marginTop: "50px" }}>
+                <FaCalendarCheck size={30} color='orange' />
                 <br></br>
-                <div className='row' >
-                  <div className="col-md-6">
-
-                    <input type="email" placeholder='Email *' className="form-control" value={FormData.Email} name="email" onChange={(e) => FormHandeller(e)} />
-                    <small className="text-danger">{FormErr.EmailErr}</small>
-                  </div>
-                  <div className="col-md-6">
-                    <input type="text" placeholder='Phone Number*' className="form-control" value={FormData.Username} name="Phone" onChange={(e) => FormHandeller(e)} />
-                    <small className="text-danger">{FormErr.PhoneErr}</small>
-                  </div>
-                </div>
-                <input type="hidden" name="selected" value={selected}/>
+                <p style={{ color: 'orange' }}>Booking Date and Time </p>
+                <h4 style={{ color: "orange" }}> {selected}</h4>
+              </div>
+              <div className='row' style={{ marginTop: "50px" }}>
+                <FaMoneyCheck size={30} color='orange' />
                 <br></br>
-                <button type="submit" className="btn btn-outline-warning">Conferm Booking</button> 
-              </form>
+                <p style={{ color: 'orange' }}>Reservation Tax</p>
+                <h4 style={{ color: 'orange' }}>{Tax} EGP</h4>
+              </div>
+            </div>
+            {/* backgroundColor: "black", */}
+            <div className='col-6' style={{ height: "400px" }}>
+              <h3 style={{ color: "white", marginTop: "20px" }}>Please input your <span style={{ color: "orange" }}>contact details</span></h3>
+              <div style={{ marginTop: "50px" }}>
+
+                <form onSubmit={(e) => handleSubmit(e)}>
+                  <div className='row'>
+                    <div className="col-6">
+
+                      <input type="text" placeholder='First Name*' className="form-control" value={FormData.Name} name="First_Name" onChange={(e) => FormHandeller(e)} />
+                      <small className="text-danger">{FormErr.First_NameErr}</small>
+                    </div>
+                    <div className="col-6">
+
+                      <input type="text" placeholder='Last Name*' className="form-control" value={FormData.Name} name="Last_Name" onChange={(e) => FormHandeller(e)} />
+                      <small className="text-danger">{FormErr.Last_NameErr}</small>
+                    </div>
+                  </div>
+                  <br></br>
+                  <div className='row' >
+                    <div className="col-md-6">
+
+                      <input type="email" placeholder='Email *' className="form-control" value={FormData.Email} name="email" onChange={(e) => FormHandeller(e)} />
+                      <small className="text-danger">{FormErr.EmailErr}</small>
+                    </div>
+                    <div className="col-md-6">
+                      <input type="text" placeholder='Phone Number*' className="form-control" value={FormData.Username} name="Phone" onChange={(e) => FormHandeller(e)} />
+                      <small className="text-danger">{FormErr.PhoneErr}</small>
+                    </div>
+                  </div>
+                  <input type="hidden" name="selected" value={selected} />
+                  <br></br>
+                  <button type="submit" className="btn btn-outline-warning">Conferm Booking</button>
+                </form>
+              </div>
             </div>
           </div>
         </div>
