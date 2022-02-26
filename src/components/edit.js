a010b10a50a0da7e5f293c5e050a36f5d8d9ec2b
Mariamadelmuhammad <95190276+Mariamadelmuhammad@users.noreply.github.com>
2022-02-26 03:24:01 +0200

    temp-profile

--- src/components/edit.js
+++ src/components/edit.js
@@ -18,7 +18,73 @@ export default function Edit() {
   const [isSubmit, setIsSubmit] = useState(false);
   const handleSubmit = (e) => {
     e.preventDefault();
-    setFormErrors(validate(FormValues));}
+    setFormErrors(validate(FormValues));
+  };
+
+  const handleChange = (e) => {
+    console.log(e.target);
+    const { name, value } = e.target;
+    setFormValues({ ...FormValues, [name]: value });
+    console.log(FormValues);
+  };
+
+  useEffect(() => {
+    console.log(FormErrors);
+    if (Object.keys(FormErrors).length === 0 && isSubmit) {
+      console.log(FormValues);
+    }
+  }, [FormErrors]);
+  const validate = (values) => {
+    const errors = {};
+    const validname = /[A-Za-z]$/;
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i;
+    const validPassword =
+      /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/;
+    const validphone = /^01[0125][0-9]{8}$/;
+
+    if (!values.firstname) {
+      errors.firstname = "Firstname is required!";
+    } else if (!validname.test(values.firstname)) {
+      errors.firstname =
+        "This is not a valid name!, Name should be charachters only";
+    }
+    if (!values.lastname) {
+      errors.lastname = "Lastname is required!";
+    } else if (!validname.test(values.lastname)) {
+      errors.lastname =
+        "This is not a valid name!, Name should be charachters only";
+    }
+    if (!values.email) {
+      errors.email = "Email is required!";
+    } else if (!regex.test(values.email)) {
+      errors.email = "This is not a valid email format!";
+    }
+    if (!values.password) {
+      errors.password = "Password is required";
+    } else if (values.password.length < 8) {
+      errors.password = "Password must be more than 8 characters";
+    } else if (validPassword.test(values.password)) {
+      errors.password =
+        "This is not a valid Password format!, Minimum eight characters, at least one letter, one number and one special character ";
+    }
+    if (!values.repeatpassword) {
+      errors.repeatpassword = "Please Repeat Your Password ";
+    } else if (values.repeatpassword != values.password) {
+      errors.repeatpassword = "Passwords Don't Match";
+    }
+    if (!values.phone) {
+      errors.phone = "Phone Number is required!";
+    } else if (!validphone.test(values.phone)) {
+      errors.phone = "This is not a valid Egyption Phone Number!";
+    }
+    if (!values.address) {
+      errors.address = "Address is required!";
+    }
+    if (!values.bdate) {
+      errors.bdate = "Birth Date is required!";
+    }
+    return errors;
+  };
 
   // const token = localStorage.getItem("token");
   // const user = jwt(token);
@@ -42,6 +108,14 @@ export default function Edit() {
             <div className="col-md-6">
               <div className="profile-head">
                 <h5 className="text-center">Mariam Adel</h5>
+                {/* username */}
+                {Object.keys(FormErrors).length === 0 && isSubmit ? (
+                  <div classNameName="ui message success">
+                    Registered successfully
+                  </div>
+                ) : (
+                  console.log(JSON.stringify(FormValues, undefined, 2))
+                )}
 
                 <ul className="nav nav-tabs" id="myTab" role="tablist">
                   <li className="nav-item">
@@ -77,12 +151,13 @@ export default function Edit() {
                     <div className="col-md-6">
                       <div class="input-group mb-3">
                         <input
-                         value ={FormValues.firstname}
                           type="text"
                           class="form-control"
                           placeholder="First Name"
                           aria-label="First Name"
                           aria-describedby="basic-addon1"
+                          value={FormValues.firstname}
+                          onChange={handleChange}
                         />
                       </div>
                     </div>
@@ -99,6 +174,8 @@ export default function Edit() {
                           placeholder="Old Password"
                           aria-label="Old Password"
                           aria-describedby="basic-addon1"
+                          value={FormValues.lastname}
+                          onChange={handleChange}
                         />
                       </div>
                     </div>
@@ -115,6 +192,8 @@ export default function Edit() {
                           placeholder="Old Password"
                           aria-label="Old Password"
                           aria-describedby="basic-addon1"
+                          value={FormValues.address}
+                          onChange={handleChange}
                         />
                       </div>
                     </div>
@@ -131,6 +210,8 @@ export default function Edit() {
                           placeholder="New Password"
                           aria-label="New Password"
                           aria-describedby="basic-addon1"
+                          value={FormValues.password}
+                          onChange={handleChange}
                         />
                       </div>
                     </div>
@@ -148,6 +229,8 @@ export default function Edit() {
                           placeholder="Confirm New Password"
                           aria-label="Confirm New Password"
                           aria-describedby="basic-addon1"
+                          value={FormValues.repeatpassword}
+                          onChange={handleChange}
                         />
                       </div>
                     </div>
@@ -165,6 +248,8 @@ export default function Edit() {
                             placeholder="E-Mail"
                             aria-label="E-Mail"
                             aria-describedby="basic-addon1"
+                            value={FormValues.email}
+                            onChange={handleChange}
                           />
                         </div>
                       </div>
@@ -182,8 +267,11 @@ export default function Edit() {
                             class="form-control"
                             placeholder="Phone Number"
                             aria-label="Phone Number"
+                            value={FormValues.phone}
+                            onChange={handleChange}
                             aria-describedby="basic-addon1"
                           />
+                          
                         </div>
                       </div>
                     </div>
