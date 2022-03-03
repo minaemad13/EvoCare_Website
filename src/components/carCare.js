0fa6adacfbc5bdde44b4652c18fd8cd3d02c77d1
Esraa-BahaaElDin <esraa.bahaaeldin99@gmail.com>
2022-03-03 23:40:34 +0200

    edit -sevices -about

--- src/components/carCare.js
+++ src/components/carCare.js
@@ -1,36 +1,83 @@
 import React from 'react';
-import "./carCar.css";
 import 'bootstrap/dist/css/bootstrap.css';
+import { useEffect } from "react";
 import { Link } from 'react-router-dom';
 import Carousel from "./service_carousel";
 import Packages from "./packages";
-function carCar() {
+import { RiRoadMapLine, RiTeamLine } from 'react-icons/ri'
+// import{FaCarAlt} from 'react-icons/fa's
+import care from "../resources/car Care1.jpeg"
+import { GiExtraTime } from 'react-icons/gi'
+import { AiOutlineCar } from 'react-icons/ai'
+const carCare = () => {
+
     return (
-        <div className="bg py-5 text-center"id='body'>
 
-        <div className="container cont py-5">
+
+        <div className="content">
+
+
+
+            <br /><br /><br /><br />
+            {/* <Carosel /> */}
             <Carousel service_id={4} />
+            <br /><br /><br />
 
-            <h2 className=" text-center mb-2">Car Car</h2>
-            <hr className="hr"></hr>
-            <div className="my-3 px-5 mx-5">
-                <p className="font">Save time and money</p>
-                <p className="my-3 px-5 mx-5 ">
-                    We are dedicated to providing quality service, customer satisfaction at a great value in multiple locations offering convenient hours  <br />
-                    Our goal is to provide our customers with the friendliest, most convenient hand car wash experience possible.
-                </p>
+
+
+            {/* <intro about service /> */}
+
+            <div className="p-5 " id='container'>
+                <div className="text-center">
+                    
+                    <h3 className="mb-3 font-bold text-white">CAR CARE</h3>
+                    <hr className="hr " />
+                    <p className="font mb-5">Car wash & detailling service</p>
+                </div>
+
+
+
+
+
+                <div className="margin">
+                    <div className="row gx-5">
+                        
+                        <div className="col-lg-4 col-md-4 col-sm-12 mb-5">
+                        <img src={care} alt="intro" className="img-fluid" style={{  width: "500",height: "500px" }}  />
+                        </div>
+                        
+                        <div className="col-lg-8 col-md-8 col-sm-12" style={{ fontSize: '15px' }}>
+                            
+                        <br /> <br /> <br /> 
+                            
+                            <p>
+                            There are two different kinds of Wash services.
+                            <br/>
+                            Jet Machine : < br />
+                            This is an external wash that uses hot water using a high pressure jet, high quality detergent & shining long lasting wax. This type of wash protects the car paint from fading that could occur due to environmental effects such as strong sunlight.
+                            <br/>
+                            Rollover Machine : < br />
+                            Say Goodbye to your worries! If you’re looking for a quick wash with high quality then Wash Rollover Machine is your answer. It takes only 6 minutes to be performed. This is an external wash that uses high pressure water, high quality detergent and shining wax with long lasting effect. The German machine is equipped with the latest technology in brushes, using soft material that is gentle on the car body yet tough on dirt and doesn’t cause any scratches to the paint.
+                            <br/>
+                            Internal wash : < br />
+                            For internal wash, we use high quality detergents that clean, protect & polish the dashboard and the car interior while also protecting it against dust and sun effect as this technology is like a dust buster. 
+                            </p>
+                        
+
+                        </div>
+
+                    </div>
+                </div>
+
             </div>
+            {/* packages/> */}
+            <Packages service_id={4} />
+
         </div>
-
-        <Packages service_id={4} />
-
-    </div>
-
-
 
 
 
     );
 }
 
-export default carCar;
+export default carCare;
--- src/components/packages.js
+++ src/components/packages.js
@@ -1,51 +1,43 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import "./packages.css";
 import PackageCard from "./package-card";
 import axios from "axios";
 function Packages(sv_id) {
-    const [error, setError] = useState(null);
-    const [packs, setPacks] = useState([])
-    useEffect(() => {
-            fetch(`http://127.0.0.1:8000/packages/${sv_id.service_id}`)
-                .then(res => res.json())
-                .then(
-                    (result) => {
-                        setPacks(result.packages);
-                    },
-                    (error) => {
-                        setError(error);
-                    }
-                )
-        }, [])
+    // const [packs, setPacks] = useState({})
+    //
     //  useEffect(() => {
     //
-    //     axios.get(`http://127.0.0.1:8000/packages/${sv_id.service_id}`)
-    //     .then((res) => setPacks(res.data))
+    //     axios.get(`http://127.0.0.1:8000/packages/${sv_id}`)
+    //     .then((res) => setPackages(res.data))
     //     .catch((err) => console.log(err))
     //
     //   }, [])
     // console.log(sv_id.service_id)
-    // const packs= [{pck_price:4000,id: 1,pck_name:"Glossy protection film"},{pck_price:6500,id: 2,pck_name:"Matte protection film"}]
+    const packs = [{ pck_price: 4000, id: 1, pck_name: "Glossy protection film" }, { pck_price: 6500, id: 2, pck_name: "Matte protection film" }]
     return (
         <div className="pricing6 py-5">
-            <div className="container cont">
-                <div className="row justify-content-center">
-                    <div className="col-md-8 text-center">
-                        <h3 className="mb-3 font-bold text-white">Packages available</h3>
-                        <h6 className="subtitle  text-light font-weight-normal">With EvoCare we offer 100% satisafaction</h6>
-                    </div>
+            <div className="container cont" >
+                <div className=" text-center">
+
+                    <h3 className="mb-3 font-bold text-white" style={{ fontWeight:"bold" }} >PACKAGES AVAILABLE</h3>
+                    <hr className="hr " />
+                    <p className="font mb-5">With EvoCare we offer 100% satisafaction</p>
+
                 </div>
+
                 <div className="row mt-4">
 
-                    {packs.map((data)=>{
-                return(
-                    <PackageCard packs = {data} service = {sv_id.service_id}/>
-                )
-            })}
+                    {packs.map((data) => {
+                        return (
+                            <PackageCard packs={data} service={sv_id.service_id} />
+                        )
+                    })}
 
                 </div>
             </div>
         </div>
-    )}
+
+    )
+}
 
 export default Packages;
