7554d6957d781133d9f5b3ce0989ae4170b16bac
John <johnmagdy@rocketmail.com>
2022-03-04 00:31:05 +0200

    removed old codes

--- src/components/packages.js
+++ src/components/packages.js
@@ -3,17 +3,29 @@ import "./packages.css";
 import PackageCard from "./package-card";
 import axios from "axios";
 function Packages(sv_id) {
-    // const [packs, setPacks] = useState({})
-    //
+    const [error, setError] = useState(null);
+    const [packs, setPacks] = useState([])
+    useEffect(() => {
+            fetch(`http://127.0.0.1:8000/packages/${sv_id.service_id}`)
+                .then(res => res.json())
+                .then(
+                    (result) => {
+                        setPacks(result.packages);
+                    },
+                    (error) => {
+                        setError(error);
+                    }
+                )
+        }, [])
     //  useEffect(() => {
     //
-    //     axios.get(`http://127.0.0.1:8000/packages/${sv_id}`)
-    //     .then((res) => setPackages(res.data))
+    //     axios.get(`http://127.0.0.1:8000/packages/${sv_id.service_id}`)
+    //     .then((res) => setPacks(res.data))
     //     .catch((err) => console.log(err))
     //
     //   }, [])
     // console.log(sv_id.service_id)
-    const packs = [{ pck_price: 4000, id: 1, pck_name: "Glossy protection film" }, { pck_price: 6500, id: 2, pck_name: "Matte protection film" }]
+    // const packs= [{pck_price:4000,id: 1,pck_name:"Glossy protection film"},{pck_price:6500,id: 2,pck_name:"Matte protection film"}]
     return (
         <div className="pricing6 py-5">
             <div className="container cont" >
