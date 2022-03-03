96a6405449b3b300a50e3bed746969fc270e8bdf
John <51532708+JohnM28@users.noreply.github.com>
2022-03-04 00:55:08 +0200

    edit -sevices -about (#15)
    * edit -sevices -about
    
    * removed old codes
    
    Co-authored-by: Esraa-BahaaElDin <esraa.bahaaeldin99@gmail.com>

--- src/components/package-card.js
+++ src/components/package-card.js
@@ -12,7 +12,7 @@ function PackageCard({packs,service}) {
     };
     return (
        <div className="col-md-6">
-                        <div className="card card-shadow border-0 mb-4">
+                        <div className="card border-warning border-2 mb-5">
                             <div className="card-body p-4 bg">
                                 <div className="d-flex align-items-center">
                                     <h5 className="font-weight-medium text-white mb-0">{packs.pck_name}</h5>
@@ -22,20 +22,19 @@ function PackageCard({packs,service}) {
                                         <div className="price-box my-3 text-light">
                                             <sup style={{color:'rgb(196,184,184)'}}>EGP</sup><span className="text-light display-5">{packs.pck_price}</span>
                                             <h6 className="font-weight-light text-light">Starting from</h6>
-                                            <button className="btn btn-outline-warning font-14 text-white p-3 btn-block mt-3"
+                                            <button className="btn btn-outline-warning rounded-pill "
                                                onClick={bookPackage}>Book Now </button>
                                         </div>
                                     </div>
                                     <div className="col-lg-7 align-self-center">
                                         <ul className="list-inline pl-3 font-14 font-weight-medium text-light">
-                                            <li className="py-2"><span>dummy data</span>
+                                            <li className="py-2"><span>Improve paint brightness,high glossy</span>
                                             </li>
-                                            <li className="py-2"><span>dummy data</span>
+                                            <li className="py-2"><span>Self-cleaning, effective anti-fouling</span>
                                             </li>
-                                            <li className="py-2"><span>dummy data</span>
+                                            <li className="py-2"><span>Good adhesion, no glue left after tearing off</span>
                                             </li>
-                                            <li className="py-2"><span>dummy data</span>
-                                            </li>
+                            
                                         </ul>
                                     </div>
                                 </div>
